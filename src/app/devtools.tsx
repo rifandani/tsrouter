265b3f357d605b54ef765190c0fbce7ac36670bc
rifandani <ipandani2505@gmail.com>
2024-06-30 22:08:34 +0700

    fix: add queryClient context to RouterProvider, don't render router devtools in prod

--- src/app/devtools.tsx
+++ src/app/devtools.tsx
@@ -1,5 +1,4 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import React from 'react';
 import { router } from './providers/router/client';
 
@@ -11,26 +10,39 @@ const ReactQueryDevtoolsProduction = React.lazy(() =>
   ),
 );
 
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+      // Lazy load in development
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      })),
+    );
+
 export function Devtools(
   props: React.ComponentProps<typeof ReactQueryDevtools> = {
     buttonPosition: 'bottom-right',
     initialIsOpen: false,
   },
 ) {
-  const [showDevtools, setShowDevtools] = React.useState(false);
+  const [showRqDevtools, setShowRqDevtools] = React.useState(false);
 
   React.useEffect(() => {
-    window.toggleRqDevtools = () => setShowDevtools((prev) => !prev);
+    window.toggleRqDevtools = () => setShowRqDevtools((prev) => !prev);
   }, []);
 
   return (
     <>
-      <TanStackRouterDevtools router={router} />
+      <React.Suspense>
+        <TanStackRouterDevtools router={router} />
+      </React.Suspense>
 
       {/* this will only be rendered in development */}
       <ReactQueryDevtools {...props} />
 
-      {showDevtools && (
+      {showRqDevtools && (
         <React.Suspense fallback={null}>
           <ReactQueryDevtoolsProduction />
         </React.Suspense>
--- src/app/providers/router/provider.tsx
+++ src/app/providers/router/provider.tsx
@@ -1,6 +1,15 @@
+import { queryClient } from '#app/providers/query/client';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './client';
 
 export function AppRouterProvider() {
-  return <RouterProvider router={router} defaultPreload="intent" />;
+  return (
+    <RouterProvider
+      router={router}
+      defaultPreload="intent"
+      context={{
+        queryClient,
+      }}
+    />
+  );
 }
